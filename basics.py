3e1df1b5d15e4df42b9d2e18f07df5f451811725
Kim Black <kimblackaz@gmail.com>
2021-09-21 13:28:04 -0600

    working logic

--- basics.py
+++ basics.py
@@ -5,8 +5,10 @@ import keyboard
 def startGame():
     over = False
     word = randomWordGen()
-    print("_ " * len(word))   
-    print("Welcome to Hangman!")
+    display = ["_"] * len(word)
+    string = printWord(display)
+    print("Welcome to Hangman! \nYou have six lives. Every incorrect guess will take away a life. Once your lives reach zero, you will lose the game.")
+    print(string)  
     correct_guess = []
     incorrect_guess = []
     return over, word, correct_guess, incorrect_guess
@@ -33,27 +35,50 @@ def enterLetter():
     while len(guess) > 1:
         guess = input("Enter your guess: ")
         if len(guess) > 1:
-            "Please enter only one letter. You cannot enter numbers or symbols."
+            print("Please enter only one letter. Do not enter numbers or symbols.")
+    guess = guess.lower()
     return guess
+
+def updateLives(incorrect_guess):
+    lives = 6
+    lives -= len(incorrect_guess)
+    return lives
     
-def runGame(word, correct_guess, incorrect_guess):
+def runGame(word, correct_guess, incorrect_guess, over):
     guess = enterLetter()
     correct = checkGuess(guess, word)
     if correct == True:
         print("Correct!")
+        print("\n")
         correct_guess.append(guess)
     else:
         print("Incorrect!")
+        print("\n")
         incorrect_guess.append(guess)
-    display = ""
+        print("Incorrect guesses: " + str(incorrect_guess))
+    lives = updateLives(incorrect_guess)
+    print("Lives: " + str(lives))
+    display = ["_"] * len(word)
     for i in range(0, len(word)):
         for j in range(0, len(correct_guess)):
             if word[i] == correct_guess[j]:
-                display += correct_guess[j]
-            else:
-                display += "_"
+                display[i] = correct_guess[j]
+    string = printWord(display)
+    print(string)
+    if str(string) == str(word[0:-1]):
+        over = True
+        print("Congrats! You win! The game will now end.")
+    if lives == 0:
+        over =  True
+        print("Uh-oh, you died! Game over.")
+    return over
+    
 
-
+def printWord(array):
+    string = ""
+    for i in range(0, len(array) - 1):
+        string += array[i]
+    return string
 
 
 def main():
@@ -65,6 +90,6 @@ def main():
     incorrect_guess = result[3]
     
     while over == False:
-        runGame(word, correct_guess, incorrect_guess)
+        over = runGame(word, correct_guess, incorrect_guess, over)
 
 main()
